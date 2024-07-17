15e7c2ace2b7e3a13bfdd862cbc0163a5544123e
SENPAIII <149959627+SENPAIIIOG@users.noreply.github.com>
2024-07-17 14:14:56 +0530

    support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -5,15 +5,13 @@ module.exports = {
     description: 'support server of this Bot',
     execute(message, args) {
         const supportServerLink = "https://discord.gg/rcBJa2GTUz";
-    
-      
-      const youtubeLink = "https://www.youtube.com/@SENPAIII0007";
+        const youtubeLink = "https://www.youtube.com/@SENPAIII0007";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              url: 'https://discord.gg/rcBJa2GTUz'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
             .setImage('https://cdn.discordapp.com/attachments/1170606500994093156/1263044707534241802/static_1.png?ex=6698cd8c&is=66977c0c&hm=b4c277b27756af653c8ba396371e53302a5694bac6cb1f81e9c2762c46280c44&')
